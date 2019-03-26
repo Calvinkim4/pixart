be8cfd130b77e9acb2591277bd7e47296e1426bc
Calvin Kim <calvinkim4@gmail.com>
2019-03-26 17:45:20 -0400

    Commit 3

--- pixart.js
+++ pixart.js
@@ -8,3 +8,13 @@ let setColorButton = document.getElementById('set-color');
 
 setColorButton.addEventListener('click', changeColor);
 setColorButton.addEventListener('mouseenter', changeColor);
+
+const createDivs = () => {
+	for (let i = 0; i < 20; i++) {
+		let div = document.createElement('div');
+		document.querySelector('body').appendChild(div);
+
+	}
+}
+
+createDivs();
