53460a9f51532ad3067fc90165de07b5adaf10f5
Calvin Kim <calvinkim4@gmail.com>
2019-03-26 17:36:28 -0400

    Commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
+function changeColor (evt) {
+	document.querySelector('.brush').style.backgroundColor = document.getElementById('color-field').value;
 
+	event.preventDefault();
+}
+
+let setColorButton = document.getElementById('set-color');
+
+setColorButton.addEventListener('click', changeColor);
